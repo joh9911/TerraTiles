60038a091646c83dee35abb9a189e688a433364b
Timothy <timothy.sit@stonybrook.edu>
2024-05-06 17:37:16 -0400

    just realized r is for water. Changed level reload to m

--- src/TerraTiles/Scenes/Level_6.ts
+++ src/TerraTiles/Scenes/Level_6.ts
@@ -37,7 +37,7 @@ export default class Level_6 extends GameScene {
             this.sceneManager.changeToScene(Level_4);
         } else if (Input.isKeyPressed('5')) {
             this.sceneManager.changeToScene(Level_5);
-        } else if (Input.isKeyPressed('r')) {
+        } else if (Input.isKeyPressed('m')) {
             this.sceneManager.changeToScene(Level_6);
         }
 
